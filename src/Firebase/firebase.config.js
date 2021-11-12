e5319bc15213b8ca483f9030a3cac14fcc80504f
DruboSaptapan <saptapan2017@gmail.com>
2021-11-13 00:03:22 +0600

    firebase updated

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,22 +1,22 @@
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-export default firebaseConfig;
-
-
 // const firebaseConfig = {
-//     apiKey: "AIzaSyAmCTlHpzUlWdItjFLpAJEC7wkBrgGfC4g",
-//     authDomain: "cyclemart-007.firebaseapp.com",
-//     projectId: "cyclemart-007",
-//     storageBucket: "cyclemart-007.appspot.com",
-//     messagingSenderId: "592876707288",
-//     appId: "1:592876707288:web:87badcbb38f22327c7e6ad"
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 // };
 
 // export default firebaseConfig;
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmCTlHpzUlWdItjFLpAJEC7wkBrgGfC4g",
+    authDomain: "cyclemart-007.firebaseapp.com",
+    projectId: "cyclemart-007",
+    storageBucket: "cyclemart-007.appspot.com",
+    messagingSenderId: "592876707288",
+    appId: "1:592876707288:web:87badcbb38f22327c7e6ad"
+};
+
+export default firebaseConfig;
